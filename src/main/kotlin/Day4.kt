704802d9e298c977b2e7df69d23e08529cacc9f3
Niklas Johansen <niklasjohansen94@gmail.com>
2022-12-04 07:06:13 +0100

    Day 4 solution

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+    println("D4P1: ${day4Part1()}")
+    println("D4P2: ${day4Part2()}")
+}
+
+fun day4Part1() = readLines("day4.txt")
+    .count { line ->
+        val (a, b) = line.toPairs()
+        a.contains(b) || b.contains(a)
+    }
+
+fun day4Part2() = readLines("day4.txt")
+    .count { line ->
+        val (a, b) = line.toPairs()
+        a.overlaps(b)
+    }
+
+private fun String.toPairs() =
+    this.split(",").map { Pair(it.substringBefore("-").toInt(), it.substringAfter("-").toInt()) }
+
+private fun Pair<Int, Int>.contains(other: Pair<Int, Int>) =
+    (first <= other.first && second >= other.second)
+
+private fun Pair<Int, Int>.overlaps(other: Pair<Int, Int>) =
+    !(first > other.second || second < other.first)
