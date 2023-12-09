a1e595511d2988cef7086b6565e7041353940a6e
Niklas Johansen <niklasjohansen94@gmail.com>
2023-12-09 02:14:38 +0100

    Fix another typo

--- src/main/kotlin/aoc2023/Day07.kt
+++ src/main/kotlin/aoc2023/Day07.kt
@@ -23,8 +23,8 @@ private fun List<Hand>.getTotalWinnings(hasJoker: Boolean = false) =
     this.sortedBy { it.getStrength(hasJoker) }.mapIndexed { rank, hand -> (rank + 1) * hand.bid }.sum()
 
 private fun Hand.getStrength(hasJokers: Boolean): Long {
-    val cardTypes = if (hasJokers) CARDS_TYPES_WITH_JOKER else CARDS_TYPES
-    val totalCardStrength = (0 until 5).sumOf { i -> cardTypes.indexOf(cards[i]) * STRENGTH_RANGES[i] }
+    val cardTypes = if (hasJokers) CARD_TYPES_WITH_JOKER else CARD_TYPES
+    val totalCardStrength = (0 until 5).sumOf { i -> cardTypes.indexOf(cards[i]) * CARD_STRENGTHS[i] }
     val handTypeStrength = getHandTypeStrength(cards, hasJokers) * 10_000_000_000
     return totalCardStrength + handTypeStrength
 }
@@ -33,11 +33,11 @@ private fun getHandTypeStrength(cards: String, hasJokers: Boolean): Int {
     if (!hasJokers) return HAND_TYPES.indexOf(getHandType(cards))
     val baseCards = cards.replace("J", "")
     val numJokers = cards.count { it == 'J' }
-    val numCardTypes = CARDS_TYPES.length.toDouble()
+    val numCardTypes = CARD_TYPES.length.toDouble()
     val numPermutations = numCardTypes.pow(numJokers.toDouble()).toInt()
     return (0 .. numPermutations).maxOf { i ->
         var wildcards = ""
-        repeat(numJokers) { j -> wildcards += CARDS_TYPES[(i / numCardTypes.pow(j) % numCardTypes).toInt()] }
+        repeat(numJokers) { j -> wildcards += CARD_TYPES[(i / numCardTypes.pow(j) % numCardTypes).toInt()] }
         HAND_TYPES.indexOf(getHandType(baseCards + wildcards))
     }
 }
@@ -47,9 +47,9 @@ private fun getHandType(cards: String) =
         .sortedByDescending { (_, group) -> group.size }
         .joinToString("") { (_, group) -> group.size.toString() }
 
-private val CARDS_TYPES = "23456789TJQKA"
-private val CARDS_TYPES_WITH_JOKER = "J23456789TQKA"
+private val CARD_TYPES = "23456789TJQKA"
+private val CARD_TYPES_WITH_JOKER = "J23456789TQKA"
 private val HAND_TYPES = arrayOf("11111", "2111", "221", "311", "32", "41", "5")
-private val STRENGTH_RANGES = arrayOf(100_000_000, 1_000_000, 10_000, 100, 1)
+private val CARD_STRENGTHS = arrayOf(100_000_000, 1_000_000, 10_000, 100, 1)
 
 private data class Hand(val cards: String, val bid: Int)
